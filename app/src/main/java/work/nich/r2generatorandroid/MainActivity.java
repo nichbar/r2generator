5a33890cc36bfadd63974d3d31a82117fdcd7280
chenjuntao <chenjuntao@ghzhushou.com>
2019-03-29 09:55:22 +0800

    Update demo

--- app/src/main/java/work/nich/r2generatorandroid/MainActivity.java
+++ app/src/main/java/work/nich/r2generatorandroid/MainActivity.java
@@ -2,6 +2,7 @@ package work.nich.r2generatorandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, R2.layout.activity_main, Toast.LENGTH_SHORT).show();
     }
 }
